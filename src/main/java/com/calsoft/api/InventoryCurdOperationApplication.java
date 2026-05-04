1d60488ae4202ba3551f7093ceb16cd1af976dd0
Gouranga Ghosh <gouranga.ghosh@calsoftinc.com>
2026-05-04 18:31:50 +0530

    first commit

---
+++ src/main/java/com/calsoft/api/InventoryCurdOperationApplication.java
@@ -0,0 +1,13 @@
+package com.calsoft.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryCurdOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryCurdOperationApplication.class, args);
+	}
+
+}
